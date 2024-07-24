705bf5884449e70282e40716d90d1955055d5fa4
mcanozgur <mcanozgur1@gmail.com>
2024-07-24 22:10:09 +0300

    feat: add datatypes and variables

---
+++ datatypes.py
@@ -0,0 +1,65 @@
+# Fundemantal Data Types 
+# int (integer), str (string), float, bool, list [], tuple(), dictionaries{}, set{} 
+
+#integer stands for integer and float stands for the floating number
+
+# when we use the integer, float and any numbers, then no need the usage of ' '.
+# '' is needed to write string. 
+
+print (2 + 4)   # sum
+print (2 - 4)   # substraction
+print (2 / 4)   # divide
+print (2 * 4)   # multiply
+print (2 ** 4)  # 2 over 4 (2^4)  
+print(5 // 4)   # remainder integer  (print 1)
+print(6 % 4)    # remaing when 6 / 4 (print 2)
+
+# type() function --> is used to ge the type of object as str (string), int (integer), float
+
+
+print (type(2 / 4))  # 0.5 so it's type float. It prints Float
+
+print (type(2 + 4))  # It prints integer
+
+
+
+a = 2 + 4         # a is a variable. The sum value of  2 and 4 is assign to "a" variable.
+
+print (type(a))   #  It prints int.
+  
+b = 2 / 4           
+
+print (type(b))   # It prints float
+
+# MATH functions: 
+
+# round (to round the integer)
+# abs (to find the absolute number)
+# bin (binary number)
+
+print (round(3.9))  # it prints 4.
+
+print (abs(-20))    # it prints 20.  
+
+# sayılarda tırnak koymana gerek yok. iny yazabilirsin ya da direk sayıyı yazdırabilirsin
+
+print (int(5)) # print 5.
+
+print (5)      # print 5.
+
+print (bin(5)) # print 101. 
+
+
+
+
+
+#operator presedence - same w/ math rules.
+
+# () bu birinci
+# **
+# * / 
+# + - 
+
+print((10-3) + 2 ** 2)
+
+
---
+++ variables.py
@@ -0,0 +1,30 @@
+# variables: 
+# variables start w/ lowecase letter
+# do not start w/ integer. 
+# in the varible name --  you can use integer inside variable name. (user_list2 etc. )
+#  _ is commonly used (snake_case). But in the variable name - is NOT used (user-name).
+
+iq = 190
+
+user_age = iq / 5
+
+a = user_age
+
+print(a)      # print 190 / 5
+
+# you can assign more than one thing in a variable using comma. 
+
+x,y,z = 5, 10, 15
+
+print(x)
+
+print(y)
+
+print(x * y + z)
+
+# for CONSTANT / STATIC things such as pi number you use uppercase in the name of variable. 
+
+PI = 3.14
+
+print (PI)
+
